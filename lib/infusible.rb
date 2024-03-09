aee11f76b432f17b939755b42fdb6e209953d442
Brooke Kuhlmann <brooke@alchemists.io>
2024-03-09 08:49:33 -0700

    Added implementation requires
    Necessary to manually require the necessary files of this implementation since Zeitwerk has been removed.
    
    Milestone: minor

--- lib/infusible.rb
+++ lib/infusible.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
+require "infusible/actuator"
+require "infusible/constructor"
+require "infusible/dependency_map"
+require "infusible/errors/duplicate_dependency"
+require "infusible/errors/invalid_dependency"
+
 # Main namespace.
 module Infusible
   METHOD_SCOPES = %i[public protected private].freeze

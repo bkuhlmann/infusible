83a687a5d55ff92aa237f651d969d194b8af3aa6
Brooke Kuhlmann <brooke@alchemists.io>
2024-08-10 10:29:34 -0600

    Removed with class method
    No longer necessary as this has been replaced by `.[]`.
    
    Milestone: major

--- spec/lib/infusible_spec.rb
+++ spec/lib/infusible_spec.rb
@@ -14,22 +14,4 @@ RSpec.describe Infusible do
       expect(child.new.inspect).to include("@a=1, @b=2, @c=3")
     end
   end
-
-  describe ".with" do
-    before { stub_const "Test::Container", {a: 1, b: 2, c: 3} }
-
-    it "answers injected dependencies" do
-      stub_const "Test::Import", infusible.with(Test::Container)
-      child = Class.new.include Test::Import[:a, :b, :c]
-
-      expect(child.new.inspect).to include("@a=1, @b=2, @c=3")
-    end
-
-    it "answers deprecation warning" do
-      expectation = proc { stub_const "Test::Import", infusible.with(Test::Container) }
-      output = "`Infusible.with` is deprecated, use `.[]` instead.\n"
-
-      expect(&expectation).to output(output).to_stderr
-    end
-  end
 end

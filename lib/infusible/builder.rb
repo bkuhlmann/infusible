cb9ed69a6eae521384c16fd5943c031337c7836a
Brooke Kuhlmann <brooke@alchemists.io>
2024-10-11 16:30:21 -0600

    Refactored builder to simplify class and instance modules
    Necessary to reduce unnecessary complexity since we don't need the _Module Builder Pattern_ for these objects.
    
    Milestone: patch

--- lib/infusible/builder.rb
+++ lib/infusible/builder.rb
@@ -27,8 +27,8 @@ module Infusible
       @container = container
       @dependencies = DependencyMap.new(*configuration)
       @scope = scope
-      @class_module = Class.new(Module).new
-      @instance_module = Class.new(Module).new.set_temporary_name "infusible-parent"
+      @class_module = Module.new
+      @instance_module = Module.new.set_temporary_name "infusible-parent"
 
       freeze
     end
